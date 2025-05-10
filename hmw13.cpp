a66278ecef272dcce2b2e3a1e77fef5ca0af6ec2
Mr Sigma <najdyuk.stas1@gmail.com>
2025-05-10 16:09:34 +0300

    ver2

--- hmw13.cpp
+++ hmw13.cpp
@@ -1,42 +1,68 @@
 ﻿#include <iostream>
-
+#include <fstream>
+#include <string>
 using namespace std;
 
-struct Vector3D {
-    float x, y, z;
-
-    Vector3D multiply(float lol) {
-        return { x * lol, y * lol, z * lol };
-    }
-
-    Vector3D add(const Vector3D& vec) {
-        return { x + vec.x, y + vec.y, z + vec.z };
-    }
-
-    Vector3D subtract(const Vector3D& vec) {
-        return { x - vec.x, y - vec.y, z - vec.z };
-    }
-
-    void print() {
-        cout << "(" << x << ", " << y << ", " << z << ")" << endl;
-    }
-};
-
 int main() {
-    Vector3D v1 = { 1.0f, 2.0f, 3.0f };
-    Vector3D v2 = { 4.0f, 5.0f, 6.0f };
+    //Task 2 
+    // 1 завдання не робив бо воно вже є в другому
+    ofstream outFile("info.txt");
+    if (outFile.is_open()) {
+        string word;
+        cout << "Enter words (to finish enter 'exit'):\n";
+        cin >> word;
+        while (word != "exit") {
+            outFile << word << " ";
+            cin >> word;
+        }
+        outFile.close();
+    }
+    else {
+        cout << "Failed to open file\n";
+    }
 
-    Vector3D sum = v1.add(v2);
-    Vector3D difference = v1.subtract(v2);
-    Vector3D scaled = v1.multiply(2.0f);
+    ifstream inFile("info.txt");
+    if (inFile.is_open()) {
+        char ch;
+        int count = 0;
 
-    cout << "Сума векторів: ";
-    sum.print();
+        while (inFile >> ch) {
+            count++; 
+        }
 
-    cout << "Різниця векторів: ";
-    difference.print();
+        inFile.close();
+        cout << "Count of symbols: " << count << endl;
+    }
+    else {
+        cout << "Failed to open file\n";
+    }
 
-    cout << "Вектор v1, помножений на 2: ";
-    scaled.print();
 
+    //Task 3
+
+    int n;
+   
+    cout << "How much nums do u want to enter? ";
+    cin >> n;
+
+    int* arr = new int[n];
+
+    cout << "Enter " << n << " nums:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    ofstream outFile("reverse.txt");
+    if (outFile.is_open()) {
+        for (int i = n - 1; i >= 0; i--) {
+            outFile << arr[i] << " ";
+        }
+        outFile.close();
+        cout << "Nums was written in reverse in 'reverse.txt'\n";
+    }
+    else {
+        cout << "Failed to open file\n";
+    }
+
+    delete[] arr;
 }
